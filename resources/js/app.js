c265af82bb77e738e21eaab0c6c280f72d871912
Matochee <164320190+Matochee@users.noreply.github.com>
2025-02-25 09:58:59 +0100

    V1.3.7 - HomePage Swiper

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import { initializeCalendar } from './calendar.js';
 import { setupModal } from './modal.js';
 import { initGSAPAnimations } from './gsapAnimations.js';
-import { initSwiper } from './swiperSetup.js';
+import { initSwiper } from './SwiperSetup.js';
 
 document.addEventListener("DOMContentLoaded", function () {
     initializeCalendar();
